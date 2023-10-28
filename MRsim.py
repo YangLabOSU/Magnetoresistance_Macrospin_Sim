912983b47095f5b400459d2314b6bcea2ba2f2c9
justinm678 <justinmichel678@gmail.com>
2023-10-27 23:45:24 -0400

    added solution object and domains

--- MRsim.py
+++ MRsim.py
@@ -57,8 +57,9 @@ class DMI:
         return 'DMI: {}, {}'.format(self.name,self.magnitude)
 
 # Class defining the free_energy for a macrospin moment
-class Free_energy:
-    def __init__(self):
+class Free_Energy:
+    def __init__(self, name):
+        self.name=name
         self.terms=[]
     def add_anisotropy(self, anisotropy: Anisotropy):
         self.terms.append(anisotropy)
@@ -69,35 +70,18 @@ class Free_energy:
     def add_DMI(self, dmi: DMI):
         self.terms.append(dmi)
 
+    def change_term_vector(self, get_name: str, new_vector: Vector3d):
+        for term in self.terms:
+            if term.name == get_name:
+                term.vector=new_vector
+
     def __repr__(self):
         rep_string='terms in free_energy:\n'
         for term in self.terms:
             rep_string+=term.__repr__()+'\n'
         return rep_string
-    
-def make_parameter_chart(f: Free_energy):
-    d=pd.DataFrame()
-    terms=[]
-    magnitudes=[]
-    for term in f.terms:
-        #for now only calculate relative phi angle
-        if isinstance(term, Exchange):
-            terms.append('Exchange')
-            magnitudes.append(term.magnitude)
-        if isinstance(term, Anisotropy):
-            terms.append('K$_{}$ Anisotropy at {}$^\circ$'.format(term.symmetry,term.vector.phi))
-            magnitudes.append(term.vector.r)
-        if isinstance(term, Zeeman):
-            terms.append('Zeeman')
-            magnitudes.append(term.vector.r)
-        if isinstance(term, DMI):
-            terms.append('DMI')
-            magnitudes.append(term.magnitude)
-    d['term']=terms
-    d['magnitude']=magnitudes
-    return d
 
-def minimize_energy(moment_list, free_energy: Free_energy, angle_guess=0):
+def minimize_energy(moment_list, free_energy: Free_Energy, angle_guess=0):
     def energy_function(x):
         E1=0
         E2=0
@@ -131,38 +115,83 @@ def minimize_energy(moment_list, free_energy: Free_energy, angle_guess=0):
     moment_list[1].vector.phi=phi_opt['x'][1]*180/np.pi
     return moment_list
 
-def calculate_angular_dependence(number_of_steps=360, I_direction=0, H_magnitude=20, savegif=''):
-    angles=np.linspace(0.01,359.999,number_of_steps)
-    m1ang=[]
-    m2ang=[]
-    # get the equillibrium positions of m1 and m2 for each field angle
-    for angle in angles:   
-        m1=Moment('m1',Vector3d(90,90,1))
-        m2=Moment('m2',Vector3d(-90,90,1))
-        Hext=Vector3d(angle,90,H_magnitude)
-        f=Free_energy()
-        f.add_anisotropy(Anisotropy('uniaxial_anisotropy',1,Vector3d(0,90,1e-4)))
-        # f.add_anisotropy(Anisotropy('biaxial_anisotropy',2,Vector3d(45,90,1e-4)))
-        f.add_anisotropy(Anisotropy('triaxial_anisotropy',3,Vector3d(60,90,2e-5)))
-        f.add_anisotropy(Zeeman('external_field',Hext))
-        f.add_anisotropy(Exchange('ex',1000))
-        f.add_anisotropy(DMI('dmi',2.2))
-        minimize_energy([m1,m2],f,angle_guess=angle)
-        m1ang.append(m1.vector.phi)
-        m2ang.append(m2.vector.phi) 
-    m1ang=np.asarray(m1ang)
-    m2ang=np.asarray(m2ang)
-    Neel_direction_rad=np.pi/180*(m1ang+m2ang)/2+np.pi/2
-    canting_angle_deg=(m1ang+m2ang)/2+90-m1ang
-    print('Canting angle average: {} deg'.format(np.mean(canting_angle_deg)))
-    spin_polarization_direction_rad=I_direction*np.pi/180+np.pi/2
+# Class that holds the result of an angular field sweep
+class Angular_Response:
 
-    # contributions to the MR from each sublattice magnetization
-    m1_Rxx_contribution=-np.cos(2*(m1ang*np.pi/180-spin_polarization_direction_rad))
-    m1_Rxy_contribution=-np.sin(2*(m1ang*np.pi/180-spin_polarization_direction_rad))
-    m2_Rxx_contribution=-np.cos(2*(m2ang*np.pi/180-spin_polarization_direction_rad))
-    m2_Rxy_contribution=-np.sin(2*(m2ang*np.pi/180-spin_polarization_direction_rad))
+    def __init__(self, number_of_steps=360, I_direction=0, H_magnitude=20):
+        self.H_magnitude=H_magnitude
+        self.I_direction=I_direction
+        self.spin_polarization_direction_rad=self.I_direction*np.pi/180+np.pi/2
+        self.angles=np.linspace(0.01,359.999,number_of_steps)
+        self.domain_free_energies=[]
+        self.domain_moment1_angles=[]
+        self.domain_moment2_angles=[]
+        self.Rxx=np.zeros(number_of_steps)
+        self.Rxy=np.zeros(number_of_steps)
     
+    def add_domain_response(self, free_energy: Free_Energy):
+        
+        free_energy.add_anisotropy(Zeeman('angular_external_field',Vector3d(0,90,self.H_magnitude)))
+        m1ang=[]
+        m2ang=[]
+        # get the equillibrium positions of m1 and m2 for each field angle
+        for angle in self.angles:   
+            m1=Moment('m1',Vector3d(90,90,1))
+            m2=Moment('m2',Vector3d(-90,90,1))
+            Hext=Vector3d(angle,90,self.H_magnitude)
+            free_energy.change_term_vector('angular_external_field',Hext)
+            minimize_energy([m1,m2],free_energy,angle_guess=angle)
+            m1ang.append(m1.vector.phi)
+            m2ang.append(m2.vector.phi) 
+        m1ang=np.asarray(m1ang)
+        m2ang=np.asarray(m2ang)
+        Neel_direction_rad=np.pi/180*(m1ang+m2ang)/2+np.pi/2
+        canting_angle_deg=(m1ang+m2ang)/2+90-m1ang
+        print('Canting angle average: {} deg'.format(np.mean(canting_angle_deg)))
+        self.domain_free_energies.append(free_energy)
+        self.domain_moment1_angles.append(m1ang)
+        self.domain_moment2_angles.append(m2ang)
+
+    def calculate_resistance_contributions(self):
+        for moment in self.domain_moment1_angles:
+            self.Rxx-=np.cos(2*(moment*np.pi/180-self.spin_polarization_direction_rad))
+            self.Rxy-=np.sin(2*(moment*np.pi/180-self.spin_polarization_direction_rad))
+        for moment in self.domain_moment2_angles:
+            self.Rxx-=np.cos(2*(moment*np.pi/180-self.spin_polarization_direction_rad))
+            self.Rxy-=np.sin(2*(moment*np.pi/180-self.spin_polarization_direction_rad))
+        
+    
+def make_parameter_chart(flist: list):
+    dlist=[]
+    namelist=[]
+    for f in flist:
+            
+        d=pd.DataFrame()
+        terms=[]
+        magnitudes=[]
+        for term in f.terms:
+            #for now only calculate relative phi angle
+            if isinstance(term, Exchange):
+                terms.append('Exchange')
+                magnitudes.append(term.magnitude)
+            if isinstance(term, Anisotropy):
+                terms.append('K$_{}$ Anisotropy at {}$^\circ$'.format(term.symmetry,term.vector.phi))
+                magnitudes.append(term.vector.r)
+            if isinstance(term, Zeeman):
+                terms.append('Zeeman')
+                magnitudes.append(term.vector.r)
+            if isinstance(term, DMI):
+                terms.append('DMI')
+                magnitudes.append(term.magnitude)
+        d['term']=terms
+        d['magnitude']=magnitudes
+        dlist.append(d)
+        namelist.append(f.name)
+    return dlist, namelist
+
+def plot_angular_SMR(angular_response: Angular_Response, savegif=''):
+    angular_response.calculate_resistance_contributions()
+    number_of_domains=len(angular_response.domain_free_energies)
     # plot results
     fig = plt.figure(figsize=(6, 6))
     ax2 = plt.subplot(212)
@@ -170,44 +199,60 @@ def calculate_angular_dependence(number_of_steps=360, I_direction=0, H_magnitude
     ax = plt.subplot(221, polar=True)
 
     #parameter table
-    d=make_parameter_chart(f)
-    # hide axes
-    # fig.patch.set_visible(False)
-    ax3.axis('off')
-    ax3.axis('tight')
-    ax3.table(cellText=d.values,colLabels=d.columns, loc='center')
-    ax3.set_title('Simulation Parameters')
+    dlist,namelist=make_parameter_chart(angular_response.domain_free_energies)
+    for i in range(number_of_domains):
+        d=dlist[i]
+        name=namelist[i]
+        # hide axes
+        # fig.patch.set_visible(False)
+        ax3.axis('off')
+        ax3.axis('tight')
+        ax3.table(cellText=d.values,colLabels=d.columns, loc='center')
+        ax3.set_title('{} Parameters'.format(name))
     # MR plot
-    ax2.plot(angles,m1_Rxx_contribution+m2_Rxx_contribution,
+    ax2.plot(angular_response.angles,angular_response.Rxx,
              label='R$_{xx}$')
-    ax2.plot(angles,m1_Rxy_contribution+m2_Rxy_contribution,
+    ax2.plot(angular_response.angles,angular_response.Rxy,
              label='R$_{xy}$')
     
-    pointxx,=ax2.plot(angles[0],m1_Rxx_contribution[0]+m2_Rxx_contribution[0],marker='o', color='b')
-    pointxy,=ax2.plot(angles[0],m1_Rxy_contribution[0]+m2_Rxy_contribution[0],marker='o', color='r')
+    pointxx,=ax2.plot(angular_response.angles[0],angular_response.Rxx[0],marker='o', color='b')
+    pointxy,=ax2.plot(angular_response.angles[0],angular_response.Rxy[0],marker='o', color='r')
     ax2.legend()
     ax2.set_xlabel(r'$\alpha$ (deg)')
 
     # moments + field animation
     ax.set_ylim(0, 1)
-    line,=ax.plot([0,angles[0]*np.pi/180],[0,1],'b')
-    line1,=ax.plot([0,m1ang[0]*np.pi/180],[0,1],'k')
-    line2,=ax.plot([0,m2ang[0]*np.pi/180],[0,1],'k')
-    line3,=ax.plot([0,I_direction*np.pi/180],[0,1],'y')
+    line,=ax.plot([0,angular_response.angles[0]*np.pi/180],[0,1],'b')
+    line3,=ax.plot([0,angular_response.I_direction*np.pi/180],[0,1],'y')
+    xdata1=[]
+    xdata2=[]
+    for j in range(number_of_domains):
+        xdata1+=[0,angular_response.domain_moment1_angles[j][0]*np.pi/180]
+        xdata2+=[0,angular_response.domain_moment2_angles[j][0]*np.pi/180]
+        
+    print(xdata1)
+    print([0,1]*number_of_domains)
+    line1,=ax.plot(xdata1,[0,1]*number_of_domains,'k')
+    line2,=ax.plot(xdata2,[0,1]*number_of_domains,'k')
 
     def frame(i):
         # print(angles[i])
-        line.set_data([0,angles[i]*np.pi/180],[0,1])
-        line1.set_data([0,m1ang[i]*np.pi/180],[0,1])
-        line2.set_data([0,m2ang[i]*np.pi/180],[0,1])
-        pointxx.set_data(angles[i],m1_Rxx_contribution[i]+m2_Rxx_contribution[i])
-        pointxy.set_data(angles[i],m1_Rxy_contribution[i]+m2_Rxy_contribution[i])
+        xdata1=[]
+        xdata2=[]
+        for j in range(number_of_domains):
+            xdata1.append([0,angular_response.domain_moment1_angles[j][i]*np.pi/180])
+            xdata2.append([0,angular_response.domain_moment2_angles[j][i]*np.pi/180])
+        line.set_data([0,angular_response.angles[i]*np.pi/180],[0,1])
+        line1.set_data(xdata1,[0,1]*number_of_domains)
+        line2.set_data(xdata2,[0,1]*number_of_domains)
+        pointxx.set_data(angular_response.angles[i],angular_response.Rxx[i])
+        pointxy.set_data(angular_response.angles[i],angular_response.Rxy[i])
 
     ax.set_rmin(0)
 
     ax.set_rmax(1.5)
     ax.set_yticklabels([])
-    anim = mplanim.FuncAnimation(fig, func=frame, frames=range(len(angles)), interval=10)
+    anim = mplanim.FuncAnimation(fig, func=frame, frames=range(len(angular_response.angles)), interval=10)
     plt.show()
 
     if savegif != '':
@@ -215,6 +260,24 @@ def calculate_angular_dependence(number_of_steps=360, I_direction=0, H_magnitude
         writergif = mplanim.PillowWriter(fps=30) 
         anim.save(f, writer=writergif)
 
+
 savefile='./Fe2O3example.gif'
-# savefile=''
-calculate_angular_dependence(I_direction=0,H_magnitude=1, savegif=savefile)
+savefile=''
+# Define angular response for alpha scan
+a=Angular_Response(I_direction=0,H_magnitude=4.5)
+# Add anisotropies and exchange for one structural domain
+f1=Free_Energy('a along x domain')
+f1.add_anisotropy(Anisotropy('uniaxial_anisotropy',1,Vector3d(0,90,1e-3)))
+# f1.add_anisotropy(Anisotropy('biaxial_anisotropy',2,Vector3d(45,90,1e-4)))
+# f1.add_anisotropy(Anisotropy('triaxial_anisotropy',3,Vector3d(60,90,2e-5)))
+f1.add_anisotropy(Exchange('ex',1000))
+# f1.add_anisotropy(DMI('dmi',2.2))
+a.add_domain_response(f1)
+
+# Add anisotropies and exchange for one structural domain
+f2=Free_Energy('a along y domain')
+f2.add_anisotropy(Anisotropy('uniaxial_anisotropy',1,Vector3d(90,90,1e-3)))
+f2.add_anisotropy(Exchange('ex',1000))
+a.add_domain_response(f2)
+
+plot_angular_SMR(a,savegif=savefile)
